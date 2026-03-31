fb7d07884a00338c0adb25da541b4c4e2fd9c600
bnnkw <bnnkw.dev@outlook.com>
2026-03-31 15:29:34 +0900

    Extract modules from main.rs to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 use std::process::{self, ExitCode};
 
 use clap::Parser;
-use cli::Args;
-use e2e_yaml::task::Tasks;
-
-mod cli;
-mod e2e_yaml;
+use e2e::cli::Args;
+use e2e::e2e_yaml::task::Tasks;
 
 #[tokio::main]
 async fn main() -> std::result::Result<ExitCode, Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let e2e_yaml = e2e_yaml::load_e2e_yaml_from_file(&args.file)?.expand();
+    let e2e_yaml = e2e::e2e_yaml::load_e2e_yaml_from_file(&args.file)?.expand();
 
     if let Some(Err(e)) = e2e_yaml
         .tasks
